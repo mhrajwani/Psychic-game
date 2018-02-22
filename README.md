bcdfbad0775e8bf38f1fb65849e0d9b8e2af76d5
mhrajwani <mo.rajwani@gmail.com>
2018-02-22 00:05:15 -0500

    DONE

---
+++ README.md
@@ -0,0 +1,4 @@
+1. The Psychic game has all requirement needed and its complete and functional
+2. All letter key pressed is changed to lowercase.
+3. if a letter is repeated in the game than alert will pop up, also the repeated key will not reduce the guesses left count and game will continue.
+4. any key pressed that is not a letter from 'a-z', an alert will pop up and will not count towards the guesses left and game is resumed.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,16 +15,21 @@ return guess.substr( Math.floor(Math.random() * 27), 1);
 var compGuess = randomLetter();
 console.log("computer guessed letter: " + compGuess);
 
-
+//assign key pressed as userGuess
 document.onkeyup = function(event) {
 var userGuess = event.key;
 console.log("user guessed letter: " + userGuess);
 
 //Make the user guess in lower case
-userGuess.toLocaleLowerCase();
+userGuess = userGuess.toLowerCase();
 
-// check user hasnt made same guess again;
 
+
+// check user hasnt made same guess again and letter not repeated
+if(guess.indexOf(userGuess) === -1  || keyGuessed.indexOf(userGuess) != -1){
+    alert("Please choose a Letter and/or Dont repeat same letter");
+}
+    else{
 if (userGuess === compGuess){
     wins++;
     keyGuessed = [];
@@ -33,17 +38,18 @@ if (userGuess === compGuess){
 
 }else if (userGuess != compGuess && guesses != 1){
     guesses--;
+    keyGuessed.push(userGuess);
 
 }else{
     losses++;
     keyGuessed = [];
     guesses = 9;
     compGuess = randomLetter();
-}
-keyGuessed.push(userGuess);
+}}
+
 
 document.getElementById("div2").innerHTML = "Wins: " + wins;
-document.getElementById("div3").innerHTML = "losses: " + losses;
+document.getElementById("div3").innerHTML = "Losses: " + losses;
 document.getElementById("div4").innerHTML = "Guesses left: " + guesses;
 document.getElementById("div5").innerHTML = "Your guesses so far : " + keyGuessed;
 
